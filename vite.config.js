ee8cdb1a34bde46d6dcb7dd9e4354af0d40e2c29
huonline <shadowwhitin@gmail.com>
2026-07-14 15:06:22 +0700

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
-  base: '/riyadul-badi-ah/', // SESUAI DENGAN NAMA REPO KAMU
+  plugins: [react()],
+  base: '/riyadul-badi-ah/',
 })
